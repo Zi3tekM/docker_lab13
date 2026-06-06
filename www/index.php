ff79e070ebd997a858833eb42d43f1d6120df354
Michal Zietek <148136190+Zi3tekM@users.noreply.github.com>
2026-06-06 22:29:16 +0200

    Add files via upload

---
+++ www/index.php
@@ -0,0 +1,8 @@
+<?php
+// strona startowa – dowód działania stosu LEMP
+echo "<h1>LEMP Stack działa poprawnie!</h1>";
+echo "<p>PHP Version: " . phpversion() . "</p>";
+
+// szczegółowe informacje o konfiguracji PHP
+phpinfo();
+?>
